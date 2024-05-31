b0e01c70e62129f29cae73897cb8a9309c0f2fc8
Baileyton <baileyton@naver.com>
2024-05-31 20:07:08 +0900

    알고리즘 1주차 3일 5, 6번 추가

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,13 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-    메모리:28520kb 시간:288ms
-    채팅방 기록 수 n을 입력, 입장을 나타내는 ENTER 후에 닉네임을 입력받는다.
-    새로운 ENTER가 나오면 채팅기록자였어도 새로 들어온 닉네임으로 처리
-    같은 ENTER 안에 있는 동일 닉네임은 카운트 1번만
+    메모리:14180kb 시간:128ms
+
 */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
-        int n = Integer.parseInt(st.nextToken());
-
-        HashSet<String> set = new HashSet<>();
 
 
     }
