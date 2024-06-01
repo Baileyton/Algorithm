b22cf07c06c53b7a59e831ad671d0cc4a1c0fdab
Baileyton <baileyton@naver.com>
2024-06-01 19:04:20 +0900

    알고리즘 1주차 4일 1,2,3,4,5,7

---
+++ src/Java알고리즘1주차/day4/끝말잇기.java
@@ -0,0 +1,61 @@
+package Java알고리즘1주차.day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+/*
+    메모리:14672kb 시간:144ms
+    단어 목록, 후보 단어 목록 배열을 저장 후
+    후보 단어 중 유효한 것 찾기
+*/
+public class 끝말잇기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int index = -1;
+        String[] words = new String[n];
+        for(int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+            if(words[i].equals("?")) index=i;
+        }
+
+        int a = Integer.parseInt(br.readLine());
+        String[] cands = new String[a];
+        for(int i = 0; i < a; i++) {
+            cands[i] = br.readLine();
+        }
+
+        // 후보 단어들을 '?'에 넣어보면서 유효성을 검사
+        for (String cand : cands) {
+            if(isValid(words, cand, index)) {
+                System.out.println(cand);
+                break;
+            }
+        }
+    }
+
+    // 끝말잇기의 유효성 검사
+    public static boolean isValid(String[] words, String cand, int index) {
+        HashSet<String> usedWords = new HashSet<>();
+        // '?' 위치에 후보 단어를 넣습니다.
+        words[index] = cand;
+        // 첫 단어의 시작을 검사
+        usedWords.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            String prevWord = words[i - 1];
+            String nextWord = words[i];
+
+            // 중복 검사
+            if (usedWords.contains(nextWord)) return false;
+
+            // 끝말잇기 규칙 검사
+            if (prevWord.charAt(prevWord.length()-1) != nextWord.charAt(0)) return false;
+
+            usedWords.add(nextWord);
+        }
+        return true;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 /*
-    메모리:14180kb 시간:128ms
+    메모리:14268kb 시간:136ms
 
 */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
 
     }
