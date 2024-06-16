5913a04f24ac67684a81d2d7b04a5bae7e1a0177
Baileyton <baileyton@naver.com>
2024-06-16 15:39:38 +0900

    알고리즘 3주차 4일 추가 문제solve:1,2,3,4

---
+++ src/Java알고리즘3주차/day4/돌게임.java
@@ -0,0 +1,35 @@
+package Java알고리즘3주차.day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+    메모리:14356kb 시간:128ms
+*/
+public class 돌게임 {
+
+    static boolean[] dp = new boolean[1001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (dp(N)) System.out.println("SK");
+        else System.out.println("CY");
+    }
+
+    public static boolean dp(int n) {
+        dp[0] = false;
+        dp[1] = true;
+        dp[2] = false;
+        dp[3] = true;
+
+        for ( int i = 4; i <= n; i++) {
+            dp[i] = !dp[i - 1] || !dp[i - 3];
+        }
+
+        return dp[n];
+    }
+}
