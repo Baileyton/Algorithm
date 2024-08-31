799db7253d1c4ff05875d6983b8bf8a49c33c817
Baileyton <baileyton@naver.com>
2024-08-31 16:35:32 +0900

    [백준] Solve: 브론즈 V 24262번 알고리즘의 수행 시간 1

---
+++ src/백준/브론즈/V/Q24262_알고리즘의수행시간1.java
@@ -0,0 +1,17 @@
+package 백준.브론즈.V;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+    메모리:14280kb 시간:96ms
+ */
+public class Q24262_알고리즘의수행시간1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
